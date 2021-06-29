98126a06dc7f49325d13d6de011143de5032d898
Alberto <ajsaenz1091@gmail.com>
2021-06-29 18:51:28 -0400

    commit tree e95de27a602a6fd4bb902dade1fe529f00632b0b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :name
+
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array) 
+        array.filter{|word| word.chars.sort_by(&:downcase).join == @name.chars.sort_by(&:downcase).join }
+    end
+end
